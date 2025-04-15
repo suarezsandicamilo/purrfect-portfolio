ae69ccdcfdbda43db6a715ba503f28ed294b40da
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2025-04-15 13:46:07 -0600

    Set theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
 //
 
+import { Link } from 'react-router';
+
 export const App = () => {
-  return <></>;
+  return (
+    <>
+      <header className="header">
+        <div className="header-container">
+          <Link to="/" className="header-item">
+            Portfolio
+          </Link>
+          <nav className="header-nav">
+            <Link to="#about" className="header-nav-item">
+              About
+            </Link>
+            <Link to="#projects" className="header-nav-item">
+              Projects
+            </Link>
+            <Link to="#contact" className="header-nav-item">
+              Contact
+            </Link>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 };
